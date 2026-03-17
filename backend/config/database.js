0dc97b1e012078f5e2902f0674e86b0449783cc9
Akshay Thombare <you@example.com>
2026-03-17 16:37:30 +0530

    fix: add ssl for neon db

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,11 +9,19 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
+
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000,
+    },
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   }
 );
